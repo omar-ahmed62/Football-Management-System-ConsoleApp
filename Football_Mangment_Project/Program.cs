889b65a599ba32532a685773be1b18a3f82d18f6
omar salah <oa360418@gmail.com>
2026-08-12 20:32:45 +0300

    Initial project setup

---
+++ Football_Mangment_Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Football_Mangment_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
