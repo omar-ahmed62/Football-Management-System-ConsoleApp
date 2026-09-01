05fd5ad78b9c8abae154405cc747957bea1f6723
omar salah <oa360418@gmail.com>
2026-09-01 10:55:24 +0300

    Add Goal class

---
+++ Football_Mangment_Project/Models/Goal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Mangment_Project.Models
+{
+    internal class Goal
+    {
+        public Player Scorer {  get; set; }
+        public int Minute {  get; set; }
+
+        public Goal(Player scorer, int minute)
+        {
+            if (scorer == null)
+                throw new ArgumentNullException("Scorer cannot be null");
+
+            if (minute < 0 || minute > 120)
+                throw new ArgumentException("Invalid minute");
+
+            this.Scorer = scorer;
+            this.Minute = minute;
+        }
+
+
+
+    }
+}
